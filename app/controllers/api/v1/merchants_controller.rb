45eb2d94595556d7e5aedc56ebee1f343f63f1d5
Carina Sweet <carina.sweet06@gmail.com>
2021-09-05 11:05:25 -0600

    Add Merchant Serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-   render json: Merchant.paginate(page: params[:page], per_page: params[:per_page])
+    render json: MerchantSerializer.new(Merchant.paginate(page: params[:page], per_page: params[:per_page]))
   end
 end
