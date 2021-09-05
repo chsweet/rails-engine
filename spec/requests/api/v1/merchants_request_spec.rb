8cf35c5c97a3351a298bb909f371cc6799123389
Carina Sweet <carina.sweet06@gmail.com>
2021-09-05 09:02:44 -0600

    Add merchant controller and first test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-end 
+  it "sends a list of 20 merchants" do
+    create_list(:merchant, 25)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(20)
+
+    merchants.each do |book|
+      expect(merchant).to have_key(:name)
+      expect(merchant[:id]).to be_an(String)
+    end
+  end
+end
