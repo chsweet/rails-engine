4c0773ec784e9d89daa21766c8d1664950bd0a8c
Carina Sweet <carina.sweet06@gmail.com>
2021-09-06 13:34:26 -0600

    Add helper methods to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,22 @@
 class Item < ApplicationRecord
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :unit_price, presence: true
-    belongs_to :merchant
-    has_many :invoice_items
-    has_many :invoices, through: :invoice_items
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 
-    self.per_page = 20
+  self.per_page = 20
+
+  def self.find_all_by_name(value)
+    Item.where("name ~* ?", value).order(:name)
+  end
+
+  def self.find_all_by_min_price(value)
+    Item.where("unit_price >= ?", value)
+  end
+
+  def self.find_all_by_max_price(value)
+    Item.where("unit_price <= ?", value.to_f)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,50 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:unit_price) }
   end
+
+  describe 'class methods' do
+    describe "::find_all_by_name" do
+      it "finds all items that match the name parameters" do
+        item_1 = create(:item, name: "Ring World Banner")
+        item_2 = create(:item, name: "Turing Tee")
+        item_3 = create(:item, name: "Dog Collar")
+        item_4 = create(:item, name: "Ring Ring Song")
+        item_5 = create(:item, name: "Slap Band")
+
+        value = 'ring'
+
+        expect(Item.find_all_by_name(value)).to eq([item_4, item_1, item_2])
+      end
+    end
+
+    describe "::find_all_by_min_price" do
+      it "returns all items that are equal to or greater than min_price parameters" do
+        item_1 = create(:item, unit_price: 4.99)
+        item_2 = create(:item, unit_price: 3.99)
+        item_3 = create(:item, unit_price: 5.99)
+        item_4 = create(:item, unit_price: 2.50)
+        item_5 = create(:item, unit_price: 6.50)
+        item_6 = create(:item, unit_price: 7.00)
+
+        value = 4.99
+
+        expect(Item.find_all_by_min_price(value)).to eq([item_1, item_3, item_5, item_6])
+      end
+    end
+
+    describe "::find_all_by_max_price" do
+      it "returns all items that are equal to or less than max_price parameters" do
+        item_1 = create(:item, unit_price: 4.99)
+        item_2 = create(:item, unit_price: 3.99)
+        item_3 = create(:item, unit_price: 5.99)
+        item_4 = create(:item, unit_price: 2.50)
+        item_5 = create(:item, unit_price: 6.50)
+        item_6 = create(:item, unit_price: 7.00)
+
+        value = 4.99
+
+        expect(Item.find_all_by_max_price(value)).to eq([item_1, item_2, item_4])
+      end
+    end
+  end
 end
