8e42523dd005399ac93a9db965460c589dbb2901
Carina Sweet <carina.sweet06@gmail.com>
2021-09-07 20:36:21 -0600

    Add revenue routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :revenue do
+        resources :merchants, only: [:index]
+        resources :items
+      end
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants/items'
       end
