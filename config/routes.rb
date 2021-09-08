9afb81ec0fcc16196d59901a74a9efd33a910d98
Carina Sweet <carina.sweet06@gmail.com>
2021-09-08 14:03:44 -0600

    Update routes to include revenue merchant show

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :revenue do
-        resources :merchants, only: [:index]
+        resources :merchants, only: [:index, :show]
         resources :items
       end
       resources :merchants, only: [:index, :show] do
