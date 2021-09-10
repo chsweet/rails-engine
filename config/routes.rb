e8c767c60ef92ac37b235c038b130b386f848474
Carina Sweet <carina.sweet06@gmail.com>
2021-09-09 19:29:18 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/api/v1/merchants/find', to: 'api/v1/merchants#find'
-  get '/api/v1/merchants/most_items', to: 'api/v1/merchants#most_items'
-
-  get '/api/v1/items/find_all', to: 'api/v1/items#find_all'
+  # get '/api/v1/merchants/find', to: 'api/v1/merchants#find'
+  # get '/api/v1/merchants/most_items', to: 'api/v1/merchants#most_items'
+  # get '/api/v1/items/find_all', to: 'api/v1/items#find_all'
 
   namespace :api do
     namespace :v1 do
@@ -13,12 +12,16 @@ Rails.application.routes.draw do
         resources :items, only: [:index]
       end
       resources :merchants, only: [:index, :show] do
-        # get '/find', to: 'merchants#find'
-        # get '/most_items', to: 'merchants#most_items'
+        collection do
+          get '/find', to: 'merchants#find'
+          get '/most_items', to: 'merchants#most_items'
+        end
         resources :items, only: [:index], controller: 'merchants/items'
       end
       resources :items do
-        # get '/find_all', to: 'items#find_all'
+        collection do
+          get '/find_all', to: 'items#find_all'
+        end
         resources :merchant, only: [:index], controller: 'items/merchants'
       end
     end
