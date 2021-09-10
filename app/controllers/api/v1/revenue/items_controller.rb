6ef514e14033ec932d851f1fe0763207acb98abc
Carina Sweet <carina.sweet06@gmail.com>
2021-09-09 21:27:39 -0600

    Update for edge case

--- app/controllers/api/v1/revenue/items_controller.rb
+++ app/controllers/api/v1/revenue/items_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Revenue::ItemsController < ApplicationController
   def index
-    if params[:quantity] && params[:quantity] != 0 && params[:quantity].empty? == false
-      render json: ItemSerializer.item_revenue(Item.items_sorted_by_revenue(params[:quantity]))
-    elsif params[:quantity].empty?
-      render status: :bad_request
-    else
+    if !params[:quantity]
       render json: ItemSerializer.item_revenue(Item.items_sorted_by_revenue(10))
+    elsif params[:quantity] != 0 && params[:quantity].empty? == false
+      render json: ItemSerializer.item_revenue(Item.items_sorted_by_revenue(params[:quantity]))
+    else
+      render status: :bad_request
     end
   end
 end
